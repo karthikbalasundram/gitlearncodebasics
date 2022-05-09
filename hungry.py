2b2359279582b5e283d91163368a4de6e0e49e67
Karthik Balasundaram <karthikbalasundram@gmail.com>
2022-05-10 00:49:02 +0530

    not inside

--- hungry.py
+++ hungry.py
@@ -1,5 +1,9 @@
 if input()=="yes":
     print("eat samosa")
     print("mayonese")
+    print("hello")
+    print("not inside")
 else:
     print("learn github")
+    print("machine learning")
+    print("data science")
